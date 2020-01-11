c67797d604009d2e957aad49ebc82b2895b0b6eb
Joyce Lin <joycelin79@gmail.com>
2020-01-11 11:38:30 -0800

    restyle building featured

--- src/components/Building.vue
+++ src/components/Building.vue
@@ -7,29 +7,29 @@
       <b-card-group deck>
           <div class="card-wrapper" v-for="(project, index) in building.featured" 
             :key="index">
-            <b-card no-body
+            <b-card overlay
+                :img-src="project.imgSrc"
                 style="max-width: 540px;"
-                class="overflow-hidden mb-3">
-
-                <template v-if="project.projectLink">
-                  <b-link :href="project.projectLink" target="_blank">
-                    <b-card-img :src="project.imgSrc"></b-card-img>  
-                  </b-link>   
-                </template>
-
-                <template v-else>
-                  <b-card-img :src="project.imgSrc"></b-card-img>
-                </template>
-
-                <div class="carousel-caption"><h1>{{project.projectTitle}}</h1></div>
+                class=" mb-3">
+                <div class="title">
+                  <template v-if="project.projectLink">
+                    <b-link :href="project.projectLink" target="_blank">
+                      <div class="building-title"><h1>{{project.projectTitle}}</h1></div>
+                    </b-link>   
+                  </template>
+                  <template v-else>
+                    <div class="building-title"><h1>{{project.projectTitle}}</h1></div>
+                  </template>
+                </div>
                 <template v-slot:footer>
-                  <small class="text-left">{{project.projectDescription}}</small>
-                    <div class="social-icons text-left">
-                      <b-link v-if="project.projectLink" :href="project.projectLink" target="_blank"><font-awesome-icon :icon="['fas', 'external-link-alt']" class="fa-lg" /></b-link>
-                      <b-link v-if="project.githubLink" :href="project.githubLink" target="_blank"><font-awesome-icon :icon="['fab', 'github']" class="fa-lg" /></b-link>
-                      <b-link v-if="project.projectArticle" :href="project.projectArticle" target="_blank"><font-awesome-icon :icon="['fab', 'medium']" class="fa-lg" /></b-link>
-                      <b-link v-if="project.projectVideo" :href="project.projectVideo" target="_blank"><font-awesome-icon :icon="['fab', 'youtube']" class="fa-lg" /></b-link>
-                    </div>
+                  <span class="text-left">{{project.projectDescription}}</span>
+                    <!-- <div class="social-icons text-left"> -->
+                    <b-list-group class="external-links">
+                      <b-list-group-item button v-if="project.projectLink"><b-link :href="project.projectLink" target="_blank"><font-awesome-icon :icon="['fas', 'external-link-alt']" /> Check it out</b-link></b-list-group-item>
+                      <b-list-group-item button v-if="project.githubLink"><b-link :href="project.githubLink" target="_blank"><font-awesome-icon :icon="['fab', 'github']" /> GitHub</b-link></b-list-group-item>
+                      <b-list-group-item button v-if="project.projectArticle"><b-link :href="project.projectArticle" target="_blank"><font-awesome-icon :icon="['fab', 'medium']" /> Medium blog</b-link></b-list-group-item>
+                      <b-list-group-item button v-if="project.projectVideo"><b-link :href="project.projectVideo" target="_blank"><font-awesome-icon :icon="['fab', 'youtube']" /> YouTube demo</b-link></b-list-group-item>
+                    </b-list-group>
                 </template>
             </b-card>
           </div>
@@ -41,7 +41,7 @@
       <b-card-group deck>
           <div class="card-wrapper" v-for="(project, index) in building.building" 
             :key="index">
-            <b-card no-body
+            <b-card no-body=""
                 style="max-width: 540px;"
                 class="overflow-hidden mb-3">
 
@@ -87,14 +87,32 @@
 }
 #building-drawer a {
   color: dimgray;
-  /* font-weight: bold; */
 }
 #building-drawer a:hover {
   text-decoration: none;
+  color: mediumaquamarine;
+}
+.title {
+  height: 100%;
+  padding: 20% 0;
+}
+.building-title {
+  color: whitesmoke;
+}
+.building-title h1:hover {
+  color: aquamarine;
+}
+.external-links {
+  margin: 10px 0;
+}
+.external-links svg {
+  margin: 0 10px;
 }
 .card-footer {
   text-align: left;
 } 
+
+
 .carousel-caption {
   margin-bottom: 30px; 
 }
@@ -110,8 +128,8 @@
 .social-icons a {
   /* display: inline-block;     
   position: relative;    
-  z-index: 1;      */
-  /* padding: 2em;     
+  z-index: 1;      
+  padding: 2em;     
   margin: -2em;  */
 }
 .social-icons a:hover {
@@ -126,7 +144,7 @@
   content: ""; 
 }
 #building-drawer img {
-  height: 400px;
+  /* height: 400px; */
   background-size: cover;
   -webkit-filter: brightness(50%);
   -webkit-transition: all 1s ease;
@@ -145,6 +163,6 @@
   justify-content: center;
 }
 #building-drawer .card-img {
-  width: initial;
+  /* width: initial; */
 }
 </style>
